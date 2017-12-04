ebb7d46c037526cdca4173ebc02524af6fcee079
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2017-12-04 10:24:43 +0100

    loop

--- first.py
+++ first.py
@@ -1,1 +1,30 @@
-print(1)
+from sympy import *
+def f1():
+	print('----------')
+	c=-20
+	dc=5
+	while c<=40:
+		f=(9.0/5)*c+32
+		print(c,f)
+		c=c+dc
+	print('----------')
+
+def f2(n):
+	print('----------')
+	for c in range(0,n):
+		f=(9.0/5)*c+32
+		print(c,f)
+	print('----------')
+
+def f3(x):
+	print('----------')
+	for c in x:
+		f=(9.0/5)*c+32
+		print(c,f)
+	print('-----------')
+
+
+f1()
+f2(10)
+x=[1,5,10]
+f3(x)
