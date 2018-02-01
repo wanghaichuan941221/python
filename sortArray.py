aec39c8b009aeb776857af3d9dbacbccc320afa0
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2018-02-02 00:01:51 +0100

    sortArray.py

--- sortArray.py
+++ sortArray.py
@@ -21,35 +21,39 @@ def merge_sort(a):
     right = merge_sort(a[mid:])
     return merge(left,right)
 
-# def swap(x,y):
-#     temp = x
-#     x=y
-#     y= temp
-#     return x,y
-#
-# def partition(a,lo,hi):
-#     pivot = a[lo]
-#     i=lo+1,j=hi
-#     while 1:
-#         while a[i]<pivot:
-#             i+=1
-#         while a[j]>pivot:
-#             j-=1
-#         if(i>=j):
-#             break
-#         a[i],a[j]=swap(a[i],a[j])
-#     a[lo],a[j]=swap(a[lo],a[j])
-#     return j
-#
-#
-# def quick_sort(a,lo,hi):
-#     if(lo<hi):
-#         mid = lo+(hi-lo)//2
-#         p=partition(a,lo,hi)
-#         quick_sort(a,lo,mid-1)
-#         quick_sort(a,mid+1,mid+1)
+def swap(x,y):
+    temp = x
+    x=y
+    y= temp
+    return x,y
 
-# a=[1,4,3,9,6,7]
-# result = merge_sort(a)
-# for i in result:
-#     print(i)
+def partition(a,lo,hi):
+    pivot = a[lo]
+    leftmark = lo+1
+    rightmark = hi
+    done = False
+    while not done:
+        while a[leftmark]<pivot:
+            leftmark+=1
+        while a[rightmark]>pivot:
+            rightmark-=1
+        if(leftmark>=rightmark):
+            done = True
+        else:
+            a[leftmark],a[rightmark]=swap(a[leftmark],a[rightmark])
+    a[lo],a[rightmark]=swap(a[lo],a[rightmark])
+    return rightmark
+
+def quick_sort(a,lo,hi):
+    if(lo<hi):
+        mid = lo+(hi-lo)//2
+        p=partition(a,lo,hi)
+        quick_sort(a,lo,mid-1)
+        quick_sort(a,mid+1,mid+1)
+        return a
+
+a=[1,4,3,9,6,7]
+result = merge_sort(a)
+quick_sort(a,0,len(a)-1)
+for i in result:
+    print(i)
